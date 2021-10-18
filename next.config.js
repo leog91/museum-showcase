d7afd14870ba0a9ebad68e6bc98fb48d53109b13
leog <leog91@gmail.com>
2021-10-18 05:09:16 +0200

    fst commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "lh3.googleusercontent.com",
+      "lh4.ggpht.com",
+      "lh5.ggpht.com",
+      "lh6.ggpht.com",
+      "lh3.ggpht.com",
+    ],
+  },
+};
